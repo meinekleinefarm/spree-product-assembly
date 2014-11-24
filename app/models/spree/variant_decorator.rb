b978fda8ebb55614361b2c39fcf891f817d0f0f2
Christoph Bünte <info@christophbuente.de>
2014-11-24 12:11:21 +0100

    Make product assemblies actuall work with 1.3

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -4,4 +4,16 @@ Spree::Variant.class_eval do
         :join_table => "spree_assemblies_parts",
         :foreign_key => "part_id", :association_foreign_key => "assembly_id"
 
+  delegate :assembly?, :to => :product
+
+  # returns the number of inventory units "on_hand" for this variant
+  def on_hand_with_assembly(reload = false)
+    if Spree::Config[:track_inventory_levels] && self.assembly?
+      self.product.on_hand
+    else
+      on_hand_without_assembly
+    end
+  end
+  alias_method_chain :on_hand, :assembly unless method_defined?(:on_hand_without_assembly)
+
 end
